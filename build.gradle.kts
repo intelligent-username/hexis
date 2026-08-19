81ce68903a25d0c12614dbf5cd399f3d2b8346cb
Varak <varakt.23@gmail.com>
2026-08-18 23:57:31 -0400

    More work ont his refactoring. Can almost push it soon, just pushing settings for now to ensure the work

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,9 @@ import com.diffplug.gradle.spotless.SpotlessExtension
 
 plugins {
     alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
     alias(libs.plugins.android.kotlin.multiplatform.library) apply false
+    alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.kotlin.multiplatform) apply false
